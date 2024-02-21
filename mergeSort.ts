4d0db85ddd9c4c8ce62824ce2dcaaa4111596631
Sasit Srirat <55717534+sasitsrirat@users.noreply.github.com>
2024-02-21 20:49:37 +0700

    Delete mergeSort.ts

--- mergeSort.ts
+++
@@ -1,8 +0,0 @@
-// src/mergeSort.ts
-
-function merge(collection1: number[], collection2: number[], collection3: number[]): number[] {
-  // Implementation of the merge function
-  // ...
-}
-
-export default merge;
