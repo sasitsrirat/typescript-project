bb4eeab4832c72b460e17c1bf1e41a2796215ba9
Sasit Srirat <55717534+sasitsrirat@users.noreply.github.com>
2024-02-21 18:52:11 +0700

    Create mergeSort.ts

---
+++ mergeSort.ts
@@ -0,0 +1,8 @@
+// src/mergeSort.ts
+
+function merge(collection1: number[], collection2: number[], collection3: number[]): number[] {
+  // Implementation of the merge function
+  // ...
+}
+
+export default merge;
